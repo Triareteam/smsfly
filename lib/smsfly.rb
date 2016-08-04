c37b385406d2a0e408264a8c46d25ee98bea2499
Serhii-Danovsky <jipebed@gmail.com>
2016-08-04 18:28:52 +0300

    0.2.6

--- lib/smsfly.rb
+++ lib/smsfly.rb
@@ -8,11 +8,19 @@ module Smsfly
   extend Configuration
   define_setting :login
   define_setting :password
-  define_setting :login, "Please configure this file  config/initializers/smsfly.rb"
-  define_setting :password, "Please configure this file  config/initializers/smsfly.rb"
+  define_setting :login, 'Please configure this file  config/initializers/smsfly.rb'
+  define_setting :password, 'Please configure this file  config/initializers/smsfly.rb'
 
-  def self.test
-    puts 'You login:' "#{Smsfly.login}"
-    puts 'You password:' "#{Smsfly.password}"
+  def self.test_connection
+
+    if Smsfly.login.length == 12
+      puts 'You login:' "#{Smsfly.login}"
+      puts 'You password:' "#{Smsfly.password}"
+    else
+      puts  'Incorrect login'
+      puts  'Login must be like 380675807873'
+      puts  'Please configure this file  config/initializers/smsfly.rb'
+    end
+
   end
 end

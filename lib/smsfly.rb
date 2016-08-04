bb280ac4b9a425b05445f5c275cb0ba29ee5b439
Serhii-Danovsky <jipebed@gmail.com>
2016-08-04 17:57:29 +0300

    0.2.5

--- lib/smsfly.rb
+++ lib/smsfly.rb
@@ -8,6 +8,8 @@ module Smsfly
   extend Configuration
   define_setting :login
   define_setting :password
+  define_setting :login, "Please configure this file  config/initializers/smsfly.rb"
+  define_setting :password, "Please configure this file  config/initializers/smsfly.rb"
 
   def self.test
     puts 'You login:' "#{Smsfly.login}"

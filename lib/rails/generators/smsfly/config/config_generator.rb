8d60079da0974d71c2c887cbb13bece2b3ef9529
Serhii-Danovsky <jipebed@gmail.com>
2016-08-04 14:28:38 +0300

    015

--- lib/rails/generators/smsfly/config/config_generator.rb
+++ lib/rails/generators/smsfly/config/config_generator.rb
@@ -3,6 +3,10 @@ module Smsfly
     class ConfigGenerator < Rails::Generators::Base
       desc 'Creates a Smsfly gem configuration file at config/smsfly.yml'
 
+      def self.source_root
+        @_smsfly_source_root ||= File.expand_path("../templates", __FILE__)
+      end
+
       def create_config_file
         template 'smsfly.yml', File.join('config', 'smsfly.yml')
       end
--- lib/smsfly/version.rb
+++ lib/smsfly/version.rb
@@ -1,3 +1,3 @@
 module Smsfly
-  VERSION = "0.1.4"
+  VERSION = "0.1.5"
 end

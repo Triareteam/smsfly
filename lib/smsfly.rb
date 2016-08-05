7e34e5b1b818bd1aac7b6925ff6dad6a70ada8d2
Serhii-Danovsky <jipebed@gmail.com>
2016-08-05 08:51:59 +0300

    0.3.1

--- lib/smsfly.rb
+++ lib/smsfly.rb
@@ -13,7 +13,7 @@ module Smsfly
 
   def self.test_connection
 
-    if Smsfly.login.length == 12 && (Smsfly.login.is_a? Integer)
+    if Smsfly.login.length == 12
       puts 'You login:' "#{Smsfly.login}"
       puts 'You password:' "#{Smsfly.password}"
       connection(Smsfly.login,Smsfly.password)

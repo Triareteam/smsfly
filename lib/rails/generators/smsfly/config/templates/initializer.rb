8b929b4ffa2bc4b10b88d0c8398f1ca7c4a23ae4
Serhii-Danovskyi <jipebed@gmail.com>
2016-10-27 09:18:05 +0300

    Update initializer.rb

--- lib/rails/generators/smsfly/config/templates/initializer.rb
+++ lib/rails/generators/smsfly/config/templates/initializer.rb
@@ -1,4 +1,4 @@
 Smsfly.configuration do |config|
-  config.login = ENV['SMS_SLY_USER'] # You login at  https://sms-fly.com/
-  config.password = ENV['SMS_SLY_PASS'] # Your password at  https://sms-fly.com/
+  config.login = ENV['SMS_FLY_USER'] # You login at  https://sms-fly.com/
+  config.password = ENV['SMS_FLY_PASS'] # Your password at  https://sms-fly.com/
 end
